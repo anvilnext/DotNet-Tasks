6ef5168df67918e0e79443ec0274a67396b0532e
anvil <chereva@ukr.net>
2019-11-21 02:27:13 +0200

    5-2 added

---
+++ ClassLibrary1/Polynom.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Polynom
+    {
+        private int degree;
+        private int[] base_arr;
+
+        public Polynom(int degree, int[] base_arr)
+        {
+            Degree = degree;
+            this.base_arr = base_arr;
+        }
+
+        public Polynom(int degree)
+        {
+            Degree = degree;
+            base_arr = new int[degree + 1];
+        }
+
+        public int Degree
+        {
+            get { return degree; }
+            set { if (value >= 0) degree = value; }
+        }
+
+        public int this[int i]
+        {
+            get { return base_arr[i]; }
+            set { base_arr[i] = value; }
+
+        }
+
+        public void Add(int a)
+        {
+            base_arr[0] += a;
+        }
+
+        public void Add(Polynom polynom)
+        {
+            int smaller = Math.Min(base_arr.Length, polynom.base_arr.Length);
+            for (int i = 0; i < smaller; i++)
+                base_arr[i] += polynom.base_arr[i];
+                
+            if (base_arr.Length < polynom.base_arr.Length)
+            {
+                Array.Resize(ref base_arr, polynom.base_arr.Length);
+                degree = base_arr.Length - 1;
+                for (int i = smaller; i < polynom.base_arr.Length; i++)
+                    base_arr[i] = polynom.base_arr[i];
+            }   
+        }
+
+        public void Subtract(int a)
+        {
+            base_arr[0] -= a;
+        }
+
+        public void Subtract(Polynom polynom)
+        {
+            int smaller = Math.Min(base_arr.Length, polynom.base_arr.Length);
+            for (int i = 0; i < smaller; i++)
+                base_arr[i] -= polynom.base_arr[i];
+
+            if (base_arr.Length < polynom.base_arr.Length)
+            {
+                Array.Resize(ref base_arr, polynom.base_arr.Length);
+                degree = base_arr.Length - 1;
+                for (int i = smaller; i < polynom.base_arr.Length; i++)
+                    base_arr[i] = polynom.base_arr[i];
+            }
+        }
+
+        public void Multiply(int a)
+        {
+            for (int i = 0; i < base_arr.Length; i++)
+            {
+                base_arr[i] *= a;
+            }
+        }
+
+        public void Multiply(Polynom polynom)
+        {
+            int oldsize = base_arr.Length;
+            degree = degree + polynom.degree;
+            Array.Resize(ref base_arr, degree + 1);
+
+            Polynom[] elem = new Polynom[polynom.base_arr.Length];
+            for (int i = 0; i < elem.Length; i++)
+            {
+                elem[i] = new Polynom(degree);
+                for (int j = 0; j < oldsize; j++)
+                {
+                    elem[i].base_arr[j + i] = base_arr[j] * polynom.base_arr[i];
+                }
+            }
+
+            for (int i = 1; i < elem.Length; i++)
+            {
+                elem[0].Add(elem[i]);
+            }
+
+            base_arr = elem[0].base_arr;
+        }
+
+        public string WritePol()
+        {
+            string res = null;
+            for (int i = degree; i >= 0; i--)
+            {
+                if ((base_arr[i] > 0) && (i != Degree) && (i != 0) && (i != 1))
+                {
+                    res += " + " + base_arr[i] + "x^" + i;
+                }
+                else if ((base_arr[i] < 0) && (i != Degree) && (i != 0) && (i != 1))
+                {
+                    res += " - " + (-1)*base_arr[i] + "x^" + i;
+                }
+                else if ((base_arr[i] != 0) && (i == Degree))
+                {
+                    res += base_arr[i] + "x^" + i;
+                }
+                else if ((base_arr[i] != 0) && (i == 0))
+                {
+                    res += " + " + base_arr[i];
+                }
+                else if ((base_arr[i] != 0) && (i == 1))
+                {
+                    res += " + " + base_arr[i] + "x";
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return res;
+        }
+    }
+}
--- DotnetTasks/Program.cs
+++ DotnetTasks/Program.cs
@@ -11,38 +11,50 @@ namespace DotnetTasks
     {
         static void Main(string[] args)
         {
-            //5-1 Array
-            Console.WriteLine("Array");
-            int n = 5, low = 11;
-            RArray a = new RArray(n, low);
-            for (int i = low - 1; i <= low + n; i++)
-            {
-                try
-                {
-                    a[i] = 1;
-                    Console.WriteLine("a["+i+"] = "+a[i]);
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    Console.WriteLine("out of range");
-                }
-            }
+            //5-2 Polynom
+            const int degree1 = 3;
+            const int degree2 = 2;
+            int[] poly1 = new int[degree1 + 1] { 3, 6, 0, 2 }; //from junior to senior so the index is becoming a degree
+            int[] poly2 = new int[degree2 + 1] { 1, 0, 1 };
+            Polynom p = new Polynom(degree1, poly1);
+            Polynom m = new Polynom(degree2, poly2);
+            p.Multiply(2);
+            p.Multiply(m);
+            Console.WriteLine(p.WritePol());
             Console.ReadLine();
+
+            //5-1 Array
+            //Console.WriteLine("Array");
+            //int n = 5, low = 11;
+            //RArray a = new RArray(n, low);
+            //for (int i = low - 1; i <= low + n; i++)
+            //{
+            //    try
+            //    {
+            //        a[i] = 1;
+            //        Console.WriteLine("a["+i+"] = "+a[i]);
+            //    }
+            //    catch (IndexOutOfRangeException)
+            //    {
+            //        Console.WriteLine("out of range");
+            //    }
+            //}
+            //Console.ReadLine();
 
             //4-2 Arithmetic progression
-            Console.WriteLine("Arithmetic progression");
-            ArProgression arith = new ArProgression(4, 2);
-            Console.WriteLine(arith.GetNItem(10));
-            Console.WriteLine(arith.GetSum(10));
-            Console.WriteLine(arith.GetAverage(10));
-            Console.ReadLine();
+            //Console.WriteLine("Arithmetic progression");
+            //ArProgression arith = new ArProgression(4, 2);
+            //Console.WriteLine(arith.GetNItem(10));
+            //Console.WriteLine(arith.GetSum(10));
+            //Console.WriteLine(arith.GetAverage(10));
+            //Console.ReadLine();
 
             //4-1 Quadrate
-            Console.WriteLine("Quadrate");
-            Quadrate quadr = new Quadrate(4);
-            Console.WriteLine(quadr.Area());
-            Console.WriteLine(quadr.Perimeter());
-            Console.ReadLine();
+            //Console.WriteLine("Quadrate");
+            //Quadrate quadr = new Quadrate(4);
+            //Console.WriteLine(quadr.Area());
+            //Console.WriteLine(quadr.Perimeter());
+            //Console.ReadLine();
         }
     }
 }
