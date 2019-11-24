d3f7a33e4101d0f9aa68317cf824aa4b05142e48
anvil <chereva@ukr.net>
2019-11-24 13:47:13 +0200

    task 6 (matrix) added

---
+++ DotnetTasks/Matrix.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetTasks
+{
+    class Matrix
+    {
+        private int[,] matrix;
+        private int Rows, Cols;
+
+        public Matrix(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+            Rows = rows;
+            Cols = cols;
+        }
+
+        public Matrix(int N)
+        {
+            matrix = new int[N, N];
+            Rows = Cols = N;
+        }
+
+        public int this[int i, int j]
+        {
+            get { return matrix[i, j]; }
+            set { matrix[i, j] = value; }
+        }
+
+        public Matrix Add(Matrix m2)
+        {
+            try
+            {
+                if ((Rows == m2.Rows) && (Cols == m2.Cols))
+                {
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Cols; j++)
+                        {
+                            matrix[i, j] += m2[i, j];
+                        }
+                    }
+                    return this;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Matrixes don't have the same size");
+                return null;
+            }
+        }
+
+        public Matrix Subtract(Matrix m2)
+        {
+            try
+            {
+                if ((Rows == m2.Rows) && (Cols == m2.Cols))
+                {
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Cols; j++)
+                        {
+                            matrix[i, j] -= m2[i, j];
+                        }
+                    }
+                    return this;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Matrixes don't have the same size");
+                return null;
+            }
+        }
+
+        public Matrix Multiply(int a)
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    matrix[i, j] *= a;
+                }
+            }
+            return this;
+        }
+
+        public Matrix Multiply(Matrix m2)
+        {
+            try
+            {
+                if (Cols == m2.Rows)
+                {
+                    Matrix res = new Matrix(Rows, m2.Cols);
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Cols; j++)
+                        {
+                            int sum = 0;
+                            for (int r = 0; r < Rows; r++)
+                            {
+                                sum += matrix[i, r] * m2[r, j];
+                            }
+                            res[i, j] = sum;
+                        }
+                    }
+                    return res;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Number of Matrix 1 columns and Matrix 2 rows don't match");
+                return null;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- DotnetTasks/Program.cs
+++ DotnetTasks/Program.cs
@@ -9,19 +9,41 @@ namespace DotnetTasks
 {
     class Program
     {
+        static Random rand = new Random();
+
         static void Main(string[] args)
         {
-            //5-2 Polynom
-            const int degree1 = 3;
-            const int degree2 = 2;
-            int[] poly1 = new int[degree1 + 1] { 3, 6, 0, 2 }; //from junior to senior so the index is becoming a degree
-            int[] poly2 = new int[degree2 + 1] { 1, 0, 1 };
-            Polynom p = new Polynom(degree1, poly1);
-            Polynom m = new Polynom(degree2, poly2);
-            p.Multiply(2);
-            p.Multiply(m);
-            Console.WriteLine(p.WritePol());
+            //6 Matrix
+            const int size = 2;
+            const int size2 = 2;
+            Matrix m = new Matrix(size);
+            Matrix n = new Matrix(size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    m[i, j] = rand.Next(10);
+                    n[i, j] = rand.Next(10);
+                }
+            }
+            m.Print();
+            Console.WriteLine();
+            n.Print();
+            Console.WriteLine();
+            m.Multiply(n).Print();
             Console.ReadLine();
+            
+            //5-2 Polynom
+            //const int degree1 = 3;
+            //const int degree2 = 2;
+            //int[] poly1 = new int[degree1 + 1] { 3, 6, 0, 2 }; //from junior to senior so the index is becoming a degree
+            //int[] poly2 = new int[degree2 + 1] { 1, 0, 1 };
+            //Polynom p = new Polynom(degree1, poly1);
+            //Polynom m = new Polynom(degree2, poly2);
+            //p.Multiply(2);
+            //p.Multiply(m);
+            //Console.WriteLine(p.WritePol());
+            //Console.ReadLine();
 
             //5-1 Array
             //Console.WriteLine("Array");
